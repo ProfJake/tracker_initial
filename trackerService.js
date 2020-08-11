7e05675afb6905d98685ed50746898bc72dd920e
Jake Levy <levyj63@rowan.edu>
2020-08-11 18:10:16 +0000

    Initial Tracker Service created

---
+++ trackerService.js
@@ -0,0 +1,26 @@
+/*trackerService.js
+
+Provides basic Activity Tracker Functionality
+Activities:  Walk/Run
+Input:  Distance (miles)
+        Weight (lbs)
+        Time (hours)
+Output: Calories Burned
+
+ Formula Sources:
+
+ https://lovandy.com/wellness/physical/calories-burned-by-walking.html AND
+ https://lovandy.com/wellness/physical/calories-burned-running.html
+*/
+
+//requires weight in kbls
+function calcWalkCal(weight, distance){
+    return 0.3 * weight * distance;
+}
+//requires weight in lbs, and distance in miles
+function calcRunCal(weight, distance){
+    return 0.63 * weight * distance;
+}
+//speed is whatever units the user wants to use
+function calcSpeed(distance, time){
+    return distance/time;
